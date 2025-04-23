a14c829fab06e5cca182707db0fd769c944d92a8
wonkotron <wonkotron@gmail.com>
2025-04-23 07:49:30 -0700

    add functionality for card decks

--- redstrike_vassal_parse_xml.py
+++ redstrike_vassal_parse_xml.py
@@ -5,7 +5,7 @@ import xmltodict
 import bson
 
 class Unit:
-    def __init__(self, parent, xml_data):
+    def __init__(self, parent, xml_data, bson_data=None):
         self.parent = parent
         self.xml_data = xml_data
 
@@ -16,7 +16,10 @@ class Unit:
 
         self.parse_unit_xml()
         # bson contained in a Unit's Command's Nation's Faction
-        self.set_image_urls(self.parent.parent.parent._bson_data)
+        if bson_data is None:
+            self.set_image_urls(self.parent.parent.parent._bson_data)
+        else:
+            self.set_image_urls(bson_data)
 
     def parse_unit_xml(self):
         # remove weird escape characters
@@ -126,6 +129,56 @@ class Faction:
             print("WARN:  Faction with no panel nations?? {0}".format(self.name))
 
 
+class Card:
+    def __init__(self, parent, xml_data, bson_data, back_png):
+        self.parent = parent
+        self._xml_data = xml_data
+        
+        self.front_png = None
+        self.front_png_url = None
+
+        self.back_png = back_png
+        self.back_png_url = None
+        self.parse_card_xml(bson_data)
+
+    def parse_card_xml(self, bson_data):
+        # as of RS89 v1.2, card data is like unit data
+        _unit = Unit(None, self._xml_data, bson_data)
+
+        # hack:  set back png then set image urls again
+        _unit.back_png = self.back_png
+        _unit.back_png_url = None
+        _unit.set_image_urls(bson_data)
+
+        self.name = _unit.name
+        self.front_png = _unit.front_png
+        self.front_png_url = _unit.front_png_url
+        self.back_png_url = _unit.back_png_url
+
+
+class Deck:
+    nato_back_png = "NATO_Card_Back.png"
+    wp_back_png = "WP_Card_Back.png"
+
+    def __init__(self, xml_data, bson_data):
+        self._xml_data = xml_data
+        self.cards = []
+        self.parse_deck_xml(bson_data)
+
+    def parse_deck_xml(self, bson_data):
+        # remove weird escape characters
+        self.name = self._xml_data.get("@entryName").replace("\\", " ")
+        cards_raw = self._xml_data["VASSAL.build.widget.ListWidget"]["VASSAL.build.widget.PieceSlot"]
+
+        for card_raw in cards_raw:
+            if "NATO" in self.name:
+                self.cards.append(Card(self, card_raw, bson_data, Deck.nato_back_png))
+            elif "WP" in self.name:
+                self.cards.append(Card(self, card_raw, bson_data, Deck.wp_back_png))
+            else:
+                print("[WARN] bad deck??")
+
+
 def extract_vassal_file(vmod_path, vmod_temp):
     # create temp directory
     if not os.path.exists(vmod_temp):
@@ -144,13 +197,23 @@ def parse_redstrike_hierarchy(buildfile_path, bson_data):
     # A VASSAL.build.module.PieceWindow (there are more than one) contains a set of
     ## VASSAL.build.widget.TabWidget, each of which might be a nation
     # In v1.2, the GameModule -> 0th PieceWindow -> TabWidget -> TabWidget is the set of Factions
-    factions_raw = data_raw["VASSAL.build.GameModule"]["VASSAL.build.module.PieceWindow"][0]["VASSAL.build.widget.TabWidget"]["VASSAL.build.widget.TabWidget"]
+    entries_raw = data_raw["VASSAL.build.GameModule"]["VASSAL.build.module.PieceWindow"][0]["VASSAL.build.widget.TabWidget"]["VASSAL.build.widget.TabWidget"]
     
     factions = []
-    for faction_raw in factions_raw:
-        factions.append(Faction(faction_raw, bson_data))
-    
-    return factions
+    decks = []
+
+    for entry_raw in entries_raw:
+        entryName = entry_raw.get("@entryName")
+
+        if not ((entryName == "Markers") or (entryName == "Cards")):
+            factions.append(Faction(entry_raw, bson_data))
+        
+        if (entryName == "Cards"):
+            decks_raw = entry_raw["VASSAL.build.widget.TabWidget"]
+            for deck_raw in decks_raw:
+                decks.append(Deck(deck_raw, bson_data))
+
+    return factions, decks
 
 
 def parse_bson(bson_path):
@@ -160,7 +223,7 @@ def parse_bson(bson_path):
     return data
             
 
-def publish_json(factions, json_path):
+def publish_faction_json(factions, json_path):
     # build dictionary representation
     data = {}
     for faction in factions:
@@ -185,6 +248,24 @@ def publish_json(factions, json_path):
         json.dump(data, json_file, indent=4)
 
 
+def publish_decks_json(decks, json_path):
+    data = {}
+
+    for deck in decks:
+        deck_dict = {}
+        for card in deck.cards:
+            card_dict = {
+                "front_png":        card.front_png,
+                "front_png_url":    card.front_png_url,
+                "back_png":         card.back_png,
+                "back_png_url":     card.back_png_url
+            }
+            deck_dict[card.name] = card_dict
+        data[deck.name] = deck_dict
+
+    with open(json_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
 def cleanup(vmod_temp):
     shutil.rmtree(vmod_temp)
 
@@ -194,12 +275,15 @@ if __name__ == "__main__":
     vmod_path = "./Red_Strike_V1_2.vmod"
     bson_path = "./CloudInfo.bson"
     buildfile_path = os.path.abspath("{0}//{1}".format(vmod_temp, "buildFile.xml"))
-    json_path = "{0}.json".format(vmod_path)
+    factions_json_path = "{0}_factions.json".format(vmod_path)
+    cards_json_path = "{0}_cards.json".format(vmod_path)
 
     extract_vassal_file(vmod_path, vmod_temp)
     bson_data = parse_bson(bson_path)
-    factions = parse_redstrike_hierarchy(buildfile_path, bson_data)
+    factions, decks = parse_redstrike_hierarchy(buildfile_path, bson_data)
     cleanup(vmod_temp)
 
-    publish_json(factions, json_path)
+    # jsons for debugging
+    publish_faction_json(factions, factions_json_path)
+    publish_decks_json(decks, cards_json_path)
     print("DONE!")
