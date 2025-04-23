ed68dbdec63cd8427cd035a5a025844a56e060ba
wonkotron <wonkotron@gmail.com>
2025-04-23 05:13:45 -0700

    initial commit of existing code

---
+++ redstrike_vassal_parse_xml.py
@@ -0,0 +1,205 @@
+import os, shutil
+from zipfile import ZipFile
+import json
+import xmltodict
+import bson
+
+class Unit:
+    def __init__(self, parent, xml_data):
+        self.parent = parent
+        self.xml_data = xml_data
+
+        self.front_png = None
+        self.front_png_url = None
+        self.back_png = None
+        self.back_png_url = None
+
+        self.parse_unit_xml()
+        # bson contained in a Unit's Command's Nation's Faction
+        self.set_image_urls(self.parent.parent.parent._bson_data)
+
+    def parse_unit_xml(self):
+        # remove weird escape characters
+        self.name = self.xml_data.get("@entryName").replace("\\", " ")
+        self._parse_unit_data()
+
+    def _parse_unit_data(self):
+        text = self.xml_data.get("#text", "").split(";")
+
+        for item in text:
+            if ".png" in item:
+                if "," in item:
+                    self.front_png, self.back_png = item.split(",")
+                    break
+                else:
+                    self.front_png = item
+                    self.back_png = None
+                    break
+
+    def set_image_urls(self, bson_data):
+        for _, file_details in bson_data.items():
+            if file_details["Name"] == self.front_png:
+                self.front_png_url = file_details["URL"]
+            if file_details["Name"] == self.back_png:
+                self.back_png_url = file_details["URL"]
+
+            if (self.front_png_url != None):
+                if (self.back_png != None):
+                    if (self.back_png_url != None):
+                        
+                        return  # both urls are set
+                else:
+                    return      # tile has no back image
+                
+
+class Command:
+    def __init__(self, parent, xml_data):
+        self.parent = parent
+        self.xml_data = xml_data
+        self.units = []
+        self.parse_command_xml()
+
+    def parse_command_xml(self):
+        # remove weird escape characters
+        self.name = self.xml_data.get("@entryName", "").replace("\\", "")
+        
+        units_raw = self.xml_data.get("VASSAL.build.widget.PieceSlot", [])
+        if type(units_raw) is list:
+            for unit_raw in units_raw:
+                self.units.append(Unit(self, unit_raw))
+        elif type(units_raw) is dict:
+            self.units.append(Unit(self, units_raw))
+        else:
+            print("ERROR:  Command with no units??  {0}".format(self.name))
+
+
+class Nation:
+    def __init__(self, parent, xml_data):
+        self.xml_data = xml_data
+        self.parent = parent
+        self.commands = []
+        self.parse_nation_xml()
+
+    def parse_nation_xml(self):
+        # remove weird escape characters
+        self.name = self.xml_data.get("@entryName").replace("\\", " ")
+
+        commands_raw = self.xml_data.get("VASSAL.build.widget.ListWidget", [])
+        if type(commands_raw) is list:
+            for command_raw in commands_raw:
+                self.commands.append(Command(self, command_raw))
+        elif type(commands_raw) is dict:
+            self.commands.append(Command(self, commands_raw))
+        else:
+            print("ERROR:  Nation with no commands??  {0}".format(self.name))
+
+
+class Faction:
+    def __init__(self, xml_data, bson_data):
+        self._xml_data = xml_data
+        self._bson_data = bson_data
+        self.nations = []
+        self.parse_faction_xml()
+
+    def parse_faction_xml(self):
+        # remove weird escape characters
+        self.name = self._xml_data.get("@entryName").replace("\\", " ")
+
+        # A VASSAL.build.module.TabWidget or VASSAL.build.module.PanelWidget may represent a Nation
+        tab_nations = self._xml_data.get("VASSAL.build.widget.TabWidget", [])
+        panel_nations = self._xml_data.get("VASSAL.build.widget.PanelWidget", [])
+
+        if type(tab_nations) is list:
+            for tab_nation in tab_nations:
+                self.nations.append(Nation(self, tab_nation))
+        elif type(tab_nations) is dict:
+            self.nations.append(Nation(self, tab_nations))
+        else:
+            print("WARN:  Faction with no tab nations?? {0}".format(self.name))
+
+        if type(panel_nations) is list:
+            for panel_nation in panel_nations:
+                self.nations.append(Nation(self, panel_nation))
+        elif type(panel_nations) is dict:
+            self.nations.append(Nation(self, panel_nations))
+        else:
+            print("WARN:  Faction with no panel nations?? {0}".format(self.name))
+
+
+def extract_vassal_file(vmod_path, vmod_temp):
+    # create temp directory
+    if not os.path.exists(vmod_temp):
+        os.mkdir(vmod_temp)
+
+    with ZipFile(vmod_path, 'r') as vmod_zip:
+        vmod_zip.extractall(vmod_temp)
+
+
+def parse_redstrike_hierarchy(buildfile_path, bson_data):
+    # actual processing
+    data_raw = None
+    with open(buildfile_path, 'r') as f:
+        data_raw = xmltodict.parse(f.read())
+
+    # A VASSAL.build.module.PieceWindow (there are more than one) contains a set of
+    ## VASSAL.build.widget.TabWidget, each of which might be a nation
+    # In v1.2, the GameModule -> 0th PieceWindow -> TabWidget -> TabWidget is the set of Factions
+    factions_raw = data_raw["VASSAL.build.GameModule"]["VASSAL.build.module.PieceWindow"][0]["VASSAL.build.widget.TabWidget"]["VASSAL.build.widget.TabWidget"]
+    
+    factions = []
+    for faction_raw in factions_raw:
+        factions.append(Faction(faction_raw, bson_data))
+    
+    return factions
+
+
+def parse_bson(bson_path):
+    data = {}
+    with open(bson_path, 'rb') as bson_file:
+        data = bson.loads(bson_file.read())
+    return data
+            
+
+def publish_json(factions, json_path):
+    # build dictionary representation
+    data = {}
+    for faction in factions:
+        faction_dict = {}
+        for nation in faction.nations:
+            nation_dict = {}
+            for command in nation.commands:
+                command_dict = {}
+                for unit in command.units:
+                    unit_dict = {
+                        "front_png":        unit.front_png,
+                        "front_png_url":    unit.front_png_url,
+                        "back_png":         unit.back_png,
+                        "back_png_url":     unit.back_png_url
+                    }
+                    command_dict[unit.name] = unit_dict
+                nation_dict[command.name] = command_dict
+            faction_dict[nation.name] = nation_dict
+        data[faction.name] = faction_dict
+    
+    with open(json_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+
+def cleanup(vmod_temp):
+    shutil.rmtree(vmod_temp)
+
+
+if __name__ == "__main__":
+    vmod_temp = "./temp"
+    vmod_path = "./Red_Strike_V1_2.vmod"
+    bson_path = "./CloudInfo.bson"
+    buildfile_path = os.path.abspath("{0}//{1}".format(vmod_temp, "buildFile.xml"))
+    json_path = "{0}.json".format(vmod_path)
+
+    extract_vassal_file(vmod_path, vmod_temp)
+    bson_data = parse_bson(bson_path)
+    factions = parse_redstrike_hierarchy(buildfile_path, bson_data)
+    cleanup(vmod_temp)
+
+    publish_json(factions, json_path)
+    print("DONE!")
