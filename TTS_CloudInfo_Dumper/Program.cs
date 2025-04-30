1c0d43cbe07c6d122b0498f43cc33c473d585acc
wonkotron <wonkotron@gmail.com>
2025-04-30 14:58:47 -0700

    dump a bson

--- TTS_CloudInfo_Dumper/Program.cs
+++ TTS_CloudInfo_Dumper/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+
+using SteamCloudFileManager;
 
 namespace TTS_GetCloudInfo_BSON {
     internal class Program {
         static void Main(string[] args) {
+            uint tts_app_id = 286160;
+
+            var remoteStorage = new RemoteStorage(tts_app_id);
+            IRemoteFile cloudinfo_bson = remoteStorage.GetFile("CloudInfo.bson");
+            File.WriteAllBytes(cloudinfo_bson.Name, cloudinfo_bson.ReadAllBytes());
+
+            Console.WriteLine("done");
         }
     }
 }
