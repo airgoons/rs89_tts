1fa0c076e2c9bb6fdc66306cf8df917c909133e0
wonkotron <wonkotron@gmail.com>
2025-04-30 14:30:00 -0700

    Initial project setup

---
+++ TTS_CloudInfo_Dumper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTS_GetCloudInfo_BSON {
+    internal class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
